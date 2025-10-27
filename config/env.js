a8451601e222267bb13648ed91420407e1f36e0a
Samuel Kuria <iamsamuelkuria@gmail.com>
2025-10-27 13:26:56 +0300

    more setup

---
+++ config/env.js
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+
+const envFile = `.env.${process.env.NODE_ENV || "development"}.local`;
+
+config({ path: envFile });
+
+export const {
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+} = process.env;
